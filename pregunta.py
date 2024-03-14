92e53798760b6601b89ffb4883f575b2f8785968
caroAM22 <caalvarezmu@unal.edu.co>
2024-03-13 23:00:47 -0500

    Update lab 5

--- pregunta.py
+++ pregunta.py
@@ -3,47 +3,28 @@ import csv
 import pandas as pd
 
 
-def create_train_dataset():
-    folder_path = "data/train"
-    output_file = "train_dataset.csv"
+def create_test_and_train_dataset():
+    path = ["/train/", "/test/"]
+    output_file = ["train_dataset.csv", "test_dataset.csv"]
     folders = ["negative", "positive", "neutral"]
-    with open(output_file, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["phrase", "sentiment"])
-    for folder in folders:
-        folder_path = "data/train/" + folder
-        with open(output_file, "a", newline="") as csvfile:
+    i = 0
+    for file in output_file:
+        with open(file, "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
+            writer.writerow(["phrase", "sentiment"])
+        for folder in folders:
+            folder_path = "data" + path[i] + folder
+            with open(file, "a", newline="") as csvfile:
+                writer = csv.writer(csvfile)
 
-            for filename in os.listdir(folder_path):
-                if filename.endswith(".txt"):
-                    file_path = os.path.join(folder_path, filename)
-                    with open(file_path, "r") as file:
-                        content = file.read()
-                        writer.writerow([content, folder])
-    return output_file
-
-
-def create_test_dataset():
-    folder_path = "data/test"
-    output_file = "test_dataset.csv"
-    folders = ["negative", "positive", "neutral"]
-    with open(output_file, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["phrase", "sentiment"])
-    for folder in folders:
-        folder_path = "data/test/" + folder
-        with open(output_file, "a", newline="") as csvfile:
-            writer = csv.writer(csvfile)
-
-            for filename in os.listdir(folder_path):
-                if filename.endswith(".txt"):
-                    file_path = os.path.join(folder_path, filename)
-                    with open(file_path, "r") as file:
-                        content = file.read()
-                        writer.writerow([content, folder])
-    return output_file
+                for filename in os.listdir(folder_path):
+                    if filename.endswith(".txt"):
+                        file_path = os.path.join(folder_path, filename)
+                        with open(file_path, "r") as f:
+                            content = f.read()
+                            writer.writerow([content, folder])
+        i += 1
+    return
 
 
-create_train_dataset()
-create_test_dataset()
+create_test_and_train_dataset()
